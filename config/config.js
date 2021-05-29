ea349c408d3910623689b70b76c51df0c68b63cf
Godinson1 <josephgodwin274@yahoo.com>
2021-05-29 17:36:58 +0100

    fix env

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST_DEV,
     dialect: process.env.DB_DIALET,
   },
   test: {
@@ -16,10 +16,10 @@ module.exports = {
     dialect: process.env.DB_DIALET,
   },
   production: {
-    username: "b1e80fe141ee70",
-    password: "95589fe1",
-    database: "heroku_c446b8670fec9ad",
-    host: "us-cdbr-east-04.cleardb.com",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALET,
   },
 };
